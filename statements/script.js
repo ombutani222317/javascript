d9df6dd957b87ade755c61f353133a3a1cc026db
dhruvirabadiya <140322854+dhruvirabadiya@users.noreply.github.com>
2024-12-21 18:54:04 +0530

    program update

---
+++ statements/script.js
@@ -0,0 +1,19 @@
+{
+    let hero="alu arjun";
+    switch (hero) {
+        case "salman khan":
+            console.log("salaman khan is my fav.. hero");
+            break;
+        case "sarukh khan":
+            console.log("sarukh khan is my fav.. hero");
+            break;
+        case "alu arjun":
+            console.log("alu arjun  is my fav.. hero");
+            break;
+        case "akshay kumar":
+            console.log("akshay kumar is my fav.. hero");
+            break;
+        default:
+            break;
+    }
+}
