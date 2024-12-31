d97e1891d11436429bf8c4032900d3334c60d4a4
dhruvirabadiya <140322854+dhruvirabadiya@users.noreply.github.com>
2024-12-31 18:07:28 +0530

    map object

---
+++ New folder/script.js
@@ -0,0 +1,17 @@
+// MAP AND SET OBJECT START :------
+
+
+// Object 
+
+
+{
+    let object = {
+        name: "om butani",
+        age: 21,
+        hobby: "Dance and drawing",
+        married: "No",
+        study: "B.tech Computer Engineering"
+    }
+    console.log(object);
+    
+}
