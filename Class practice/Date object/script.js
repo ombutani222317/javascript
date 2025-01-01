3a598cb10f13ccebda97a95045398cd64659ecee
dhruvirabadiya <140322854+dhruvirabadiya@users.noreply.github.com>
2025-01-01 19:21:45 +0530

    math and date object

---
+++ Class practice/Date object/script.js
@@ -0,0 +1,24 @@
+// Date object start
+
+// Date object
+// 1.Date get object
+// 2.Date set object
+
+
+// 1.Date get object 
+
+{
+    let date = new Date();
+    date.getDate();
+    date.getDay();
+    date.getFullYear();
+    date.getHours();
+    date.getMilliseconds();
+    date.getMinutes();
+    date.getMonth();
+    date.getSeconds();
+    date.getTime();
+
+    console.log(date);
+
+}
---
+++ Class practice/Math method/script.js
@@ -0,0 +1,119 @@
+// Math start
+
+
+// 1. Math.PI
+// it generate PI value for any equation
+{
+    let math = Math.PI
+    console.log(math);
+
+}
+
+// 2.Math.abs()
+// it is used for any negative and positive number in positive i can not be a negative number
+
+{
+    let math = Math.abs(10);
+    console.log(math);
+}
+
+// 3.Math.trunc()
+// Math.trunc(x) returns the integer part of x:
+
+{
+    let math = Math.trunc(10);
+    console.log(math);
+}
+
+// 4.Math.random()
+// it can be used for round figure number
+
+{
+    let math = Math.round(10.42313456546312);
+    console.log(math);
+}
+
+// 5.Math.floor()
+// Math.floor(x) returns the value of x rounded down to its nearest integer:
+
+{
+    let math = Math.floor(11.9);
+    console.log(math);
+}
+
+// 6.Math.ceil()
+// Math.ceil(x) returns the value of x rounded up to its nearest integer:
+
+{
+    let math = Math.ceil(10.9);
+    console.log(math);
+}
+
+// 7.Math.sign()
+// Math.sign(x) returns if x is negative, null or positive:
+
+{
+    let math = Math.sign(-10);
+    console.log(math);
+}
+
+// 8.Math.pow()
+// Math.pow(x, y) returns the value of x to the power of y:
+
+{
+    let math = Math.pow(2, 5);
+    console.log(math);
+}
+
+// 9.Math.sqrt()
+// Math.sqrt(x) returns the square root of x:
+
+{
+    let math = Math.sqrt(25);
+    console.log(math);
+}
+
+// 10.Math.sin()
+// Math.sin(x) returns the sine (a value between -1 and 1) of the angle x (given in radians).If you want to use degrees instead of radians, you have to convert degrees to radians: **Angle in radians = Angle in degrees x PI / 180.**
+
+{
+    let math = Math.sin(0 * Math.PI / 180);
+    console.log(math);
+}
+
+// 11.Math.cos()
+// Math.cos(x) returns the cosine (a value between -1 and 1) of the angle x (given in radians).If you want to use degrees instead of radians, you have to convert degrees to radians: **Angle in radians = Angle in degrees x PI / 180.**
+
+{
+    let math = Math.cos(0 * Math.PI / 180);
+    console.log(math);
+}
+
+// 12.math.min() & math.max()
+// it can be used for to find the min max number in all over value 
+
+{
+    // max value
+    let math = Math.max(10, 20, 0, 2, 87);
+    console.log(math);
+
+    // min value 
+    let math1 = Math.min(10, 20, 0, 2, 87);
+    console.log(math1);
+}
+
+// 13.Math.log() 
+// Math.log(x) returns the natural logarithm of x.
+
+{
+    let math = Math.log(10);
+    console.log(math);
+
+    let math2 = Math.log10(1);
+    console.log(math2);
+
+    let math3 = Math.log2(11);
+    console.log(math3);
+
+}
+
